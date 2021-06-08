9cc5816c24a7bb95c120e2e01969235c748f676a
Alexandria <72262425+Harini2034@users.noreply.github.com>
2021-06-08 16:09:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function sabrina()
+{
+    window.location = ("sabrina.html");
+}
+
+function selena()
+{
+    window.location = ("selena.html");
+}
+
+function justin()
+{
+    window.location = ("justin.html");
+}
+
+function hailey()
+{
+    window.location = ("hailey.html");
+}
+
